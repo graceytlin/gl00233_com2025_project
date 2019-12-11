38af42a9645f68ed2ff42095dcd7892487b0aafc
Grace <gl00233@surrey.ac.uk>
2019-12-11 19:12:42 +0000

    Home page and meta data

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
+    assert_select 'title', 'A Day\'s Work'
+    assert_select 'h1', 'A Day\'s Work'
+    assert_select 'p', 'Hello, good sire'
   end
 
 end
